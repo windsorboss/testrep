d06e4bc0f6108bedf57592e0077ac2bd5fd0ab0a
wx <wx@wx-pc>
2015-03-31 22:51:45 +0800

    测试提交文件

---
+++ testgit/src/testgit/test.java
@@ -0,0 +1,25 @@
+/**
+ * test.java
+ * 功能：
+ * 变更履历
+ * 2015年3月31日 下午9:55:25	wx	创建
+ */
+package testgit;
+
+/**
+ * <p></p>
+ * @author wx
+ * @since
+ * @see
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word 111");
+	}
+
+}
