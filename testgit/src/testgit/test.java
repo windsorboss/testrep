fa72c45962cb2b6db269f17011444ca8074ab17c
windsorboss <windsorboss@foxmail.com>
2015-11-29 22:02:45 +0800

    test add out 1

--- testgit/src/testgit/test.java
+++ testgit/src/testgit/test.java
@@ -19,6 +19,7 @@ public class test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello word 1");
 		System.out.println("hello word 2");
 	}
 
